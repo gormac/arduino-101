314e822656ce829c5124adf6c12172aebeb06775
Marco Glasbergen <marco.glasbergen@avivasolutions.nl>
2018-01-25 21:54:40 +0100

    Added example Secrets.h settings header file

---
+++ libraries/ExampleSecrets.h
@@ -0,0 +1,8 @@
+// MQTT settings
+const char* MQTT_SERVER = "192.168.1.3";
+const char* MQTT_USERNAME = "";
+const char* MQTT_PASSWORD = "";
+
+// WiFi settings
+const char* WIFI_SSID = "some_ssid";
+const char* WIFI_PASS = "some_pass";
